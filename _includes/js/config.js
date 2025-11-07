118dcb4a3f8897d5fbc0699d17c82e2a620ec9ef
Morten Høfft <6759267+MortenHofft@users.noreply.github.com>
2025-11-07 11:30:28 +0100

    update to version 3 config

--- _includes/js/config.js
+++ _includes/js/config.js
@@ -1,105 +1,190 @@
-var siteTheme = gbifReactComponents.themeBuilder.extend({
-  baseTheme: 'light', extendWith: {
-    primary: themeStyle.colors.primary,
-    fontSize: '16px'
-  }
-});
-
 var siteConfig = {
-  version: 2,
-  routes: {
-    alwaysUseHrefs: true, // Update - there now is translations. since the site isn't translated we can use push for now. if true, then we will always use hrefs, if false we will use onClick events and push state to the history. I've added this because I just realize that the language picker doesn't work with pushState as the url of the translated site is not updated with the new url
-    enabledRoutes: ['occurrenceSearch', 'datasetSearch', 'datasetKey', 'publisherSearch', 'publisherKey', 'literatureSearch']
-  },
-  occurrence: {
-    // excludedFilters: ['occurrenceStatus', 'networkKey', 'hostingOrganizationKey', 'protocol', 'publishingCountryCode', 'institutionCode', 'collectionCode'],
-    // highlightedFilters: ['taxonKey',  'catalogNumber', 'recordedBy', 'identifiedBy'],
-    // defaultTableColumns: ['features', 'institutionKey', 'collectionKey', 'catalogNumber', 'country', 'year', 'recordedBy', 'identifiedBy'],
-    mapSettings: {
-      lat: -45.373733938343776,
-      lng: -65.8143857581085,
-      zoom: 3.5
+  "version": 3,
+  "pages": [
+    {
+      "id": "occurrenceSearch"
     },
-    // You probably need help to configure the scope - so just ask
-    // for his demo site we only show Fungi (taxonKey=5). It use the predicate structure known from GBIF download API. 
-    // See https://www.gbif.org/developer/occurrence (long page without enough anchors - search for "Occurrence Download Predicates")
-    // The format is however slightly different, in that is use camelCase for keys instead of CONSTANT_CASE. 
-    rootPredicate: {
-      type: 'or',
-      predicates: [
+    {
+      "id": "datasetSearch"
+    },
+    {
+      "id": "datasetKey"
+    },
+    {
+      "id": "publisherSearch"
+    },
+    {
+      "id": "publisherKey"
+    },
+    {
+      "id": "literatureSearch"
+    }
+  ],
+  "disableInlineTableFilterButtons": false,
+  "availableCatalogues": [
+    "OCCURRENCE",
+    "DATASET",
+    "PUBLISHER"
+  ],
+  "dataHeader": {
+    "enableApiPopup": false,
+    "enableInfoPopup": false
+  },
+  "theme": {
+    "primary": themeStyle.colors.primary,
+    "borderRadius": 3,
+    "stickyOffset": "0px"
+  },
+  "maps": {
+    "locale": "es",
+    "mapStyles": {
+      "defaultProjection": "MERCATOR",
+      "defaultMapStyle": "BRIGHT",
+      "options": {
+        "ARCTIC": [
+          "NATURAL",
+          "BRIGHT"
+        ],
+        "PLATE_CAREE": [
+          "NATURAL",
+          "BRIGHT",
+          "DARK"
+        ],
+        "MERCATOR": [
+          "NATURAL",
+          "BRIGHT",
+          "DARK"
+        ],
+        "ANTARCTIC": [
+          "NATURAL",
+          "BRIGHT",
+          "DARK"
+        ]
+      }
+    }
+  },
+  "languages": [
+    {
+      "code": "es",
+      "localeCode": "es",
+      "label": "Spanish",
+      "default": true,
+      "textDirection": "ltr",
+      "cmsLocale": "es",
+      "vocabularyLocale": "es-ES",
+      "iso3LetterCode": "spa",
+      "gbifOrgLocalePrefix": "/es",
+      "grSciCollLocalePrefix": "/es",
+      "mapTileLocale": "es"
+    },
+    {
+      "code": "en",
+      "localeCode": "en",
+      "label": "English",
+      "default": false,
+      "textDirection": "ltr",
+      "iso3LetterCode": "eng",
+      "cmsLocale": "en-GB",
+      "vocabularyLocale": "en",
+      "gbifOrgLocalePrefix": "",
+      "mapTileLocale": "en"
+    }
+  ],
+  "messages": {},
+  "occurrenceSearch": {
+    "scope": {
+      "type": "or",
+      "predicates": [
         {
-          type: 'in',
-          key: 'publishingCountry',
-          values: ['AR', 'FK', 'GS']
+          "type": "in",
+          "key": "publishingCountry",
+          "values": [
+            "AR",
+            "FK",
+            "GS"
+          ]
         },
         {
-          type: 'and',
-          predicates: [
+          "type": "and",
+          "predicates": [
             {
-              type: 'in',
-              key: 'country',
-              values: ['AR', 'FK', 'GS']
+              "type": "in",
+              "key": "country",
+              "values": [
+                "AR",
+                "FK",
+                "GS"
+              ]
             },
             {
-              type: 'equals',
-              key: 'hasGeospatialIssue',
-              value: 'false'
+              "type": "equals",
+              "key": "hasGeospatialIssue",
+              "value": "false"
             }
           ]
         },
         {
-          type: 'within',
-          key: 'geometry',
-          value: 'POLYGON((-74 -60, -25 -60, -25 -90, -74 -90, -74 -60))'
+          "type": "within",
+          "key": "geometry",
+          "value": "POLYGON((-74 -60, -25 -60, -25 -90, -74 -90, -74 -60))"
         }
       ]
+    },
+    "mapSettings": {
+      "lat": -45.373733938343776,
+      "lng": -65.8143857581085,
+      "zoom": 3.5
     }
   },
-  dataset: {
-    rootFilter: {
-      publishingCountry: 'AR'
+  "collectionSearch": {},
+  "institutionSearch": {},
+  "datasetSearch": {
+    "scope": {
+      "publishingCountry": "AR"
     },
-    excludedFilters: ['publishingCountryCode'],
+    "excludedFilters": [
+      "publishingCountry"
+    ]
   },
-  publisher: {
-    rootFilter: {
-      country: 'AR'
+  "publisherSearch": {
+    "scope": {
+      "country": "AR"
     },
-    excludedFilters: ['countrySingle'],
+    "excludedFilters": [
+      "country"
+    ]
   },
-  literature: {
-    rootFilter: {
-      predicate: {
-        type: 'or', predicates: [
-          {
-            type: 'in',
-            key: 'countriesOfResearcher',
-            values: ['AR', 'FK', 'GS']
-          },
-          {
-            type: 'in',
-            key: 'countriesOfCoverage',
-            values: ['AR', 'FK', 'GS']
-          }
-        ]
-      }
+  "literatureSearch": {
+    "scope": {
+      "type": "or",
+      "predicates": [
+        {
+          "type": "in",
+          "key": "countriesOfResearcher",
+          "values": [
+            "AR",
+            "FK",
+            "GS"
+          ]
+        },
+        {
+          "type": "in",
+          "key": "countriesOfCoverage",
+          "values": [
+            "AR",
+            "FK",
+            "GS"
+          ]
+        }
+      ]
     },
-    highlightedFilters: ['q', 'countriesOfResearcher', 'countriesOfCoverage', 'year'],
-    },
-    availableCatalogues: ['OCCURRENCE', 'DATASET', 'PUBLISHER'],
- 
-  
-  maps: {
-    locale: 'es',
-    defaultProjection: 'MERCATOR',
-    defaultMapStyle: 'BRIGHT',
-    mapStyles: {
-      ARCTIC: ['NATURAL', 'BRIGHT'],
-      PLATE_CAREE: ['NATURAL', 'BRIGHT', 'DARK'],
-      MERCATOR: ['NATURAL', 'BRIGHT', 'DARK'],
-      ANTARCTIC: ['NATURAL', 'BRIGHT', 'DARK']
-    }
-  },
+    "highlightedFilters": [
+      "q",
+      "countriesOfResearcher",
+      "countriesOfCoverage",
+      "year"
+    ]
+  }
 };
 
 // example of a language specific route overwrite
